ebb18818e8fc55564bc3b55174f54364d6caee79
Rufles69 <martinroblesjra69@gmail.com>
2025-10-01 20:10:38 -0500

    Nuevo Juego!!

---
+++ games/ninja-sombra.js
@@ -0,0 +1,9 @@
+<div class="game-card" data-game="ninja-sombra">
+    <div class="game-icon">🥷</div>
+    <h3>El Ninja Sombra</h3>
+    <h5>En desarrollo</h5>
+    <div class="game-info">
+        <div class="high-score">Mejor Nvl: <span id="ninja-sombra-highscore">0</span></div>
+        <div class="stars-earned">⭐ <span id="ninja-sombra-stars">0</span></div>
+    </div>
+</div>
